9f989850cd1e5b616372501a3a56d28c5641eba7
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2024-08-18 22:05:44 -0300

    feat: create socket communication between client and server with multiple connections

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace ChatClient
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            const int port = 8080;
+            
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            clientSocket.Connect(ipAddress, port);
+            Console.WriteLine("Connected to server.");
+            
+            Console.Write("Write your nickname: ");
+            // Read our name
+            string clientName = Console.ReadLine();
+            // Converting our name in bytes to send to server
+            byte[] clientNameBytes = Encoding.UTF8.GetBytes(clientName);
+            
+            // Sending name to server
+            clientSocket.Send(clientNameBytes);
+
+            while (true)
+            {
+                // Send datas
+                Console.Write("Message: ");
+                string message = Console.ReadLine();
+                if (message == null)
+                    break;
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                clientSocket.Send(messageBytes);
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,83 @@
-﻿namespace ChatServer
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatServer
 {
     internal class Program
     {
+        private static readonly List<Socket> ConnectedClients = new List<Socket>();
+        
         public static void Main(string[] args)
         {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            const int port = 8080;
+
+            serverSocket.Bind(new IPEndPoint(ipAddress, port));
+            
+            serverSocket.Listen(10);
+            Console.WriteLine("Waiting for a connection...");
+
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                
+                ConnectedClients.Add(clientSocket);
+
+                Thread clientThread = new Thread(() => HandleClient(clientSocket));
+                clientThread.Start();
+            }
+        }
+
+        private static void HandleClient(Socket clientSocket)
+        {
+
+            string clientName = string.Empty;
+
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int message = clientSocket.Receive(buffer);
+                clientName = Encoding.UTF8.GetString(buffer, 0, message);
+                Console.WriteLine($"{clientName} Connected.");
+
+                while (true)
+                {
+                    message = clientSocket.Receive(buffer);
+
+                    if (message == 0)
+                        break;
+                    
+                    string messageFromClient = Encoding.UTF8.GetString(buffer, 0, message);
+                    
+                    Console.WriteLine($"{clientName}: {messageFromClient}");
+                    BroadcastMessage(messageFromClient, clientSocket);
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine($"{clientSocket.RemoteEndPoint} Disconnected.");
+            }
+            
+            clientSocket.Close();
+        }
+
+        static void BroadcastMessage(string message, Socket excludeSocket)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            foreach (Socket client in ConnectedClients)
+            {
+                if (client != excludeSocket)
+                {
+                    client.Send(messageBytes);
+                }
+            }
         }
     }
 }
